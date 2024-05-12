a46bd950450539fa0fc3c62d9c048e81dab10488
Alexey Shabarshov <shabarshov3228@mail.ru>
2024-05-12 05:04:39 +0300

    refactor: removed comments

--- src/components/SearchForm/SearchFormProps.ts
+++ src/components/SearchForm/SearchFormProps.ts
@@ -3,8 +3,6 @@ import type { EmploymentForm } from "types/EmploymentForm"
 import type { Position } from "types/Position"
 
 interface SearchFormProps {
-  // form: EmploymentForm | null
-  // position: string | null
   setForm: Dispatch<SetStateAction<EmploymentForm | null>>
   setPosition: Dispatch<SetStateAction<Position | null>>
 }
